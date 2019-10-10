552b23a74448c51daef5818ad561b4723b31ec9f
youling4438 <youling0106@qq.com>
2019-10-10 23:48:12 +0800

    :ok_hand: add api for shici

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var insertShici = require("../service/shici.js").insertDocumentForShici;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+    res.send("respond with a resource");
+});
+
+router.get("/shici", function(req, res, next) {
+    // res.send('respond with a resource');
+    insertShici({ name: "Roy", age: 27 });
 });
 
 module.exports = router;
---
+++ service/shici.js
@@ -0,0 +1,51 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017";
+
+const dbName = "shici";
+
+const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const insertDocument = (db, shici, cb) => {
+    const collection = db.collection("jrsc");
+    collection.insertOne(shici, (err, result) => {
+        if (err) {
+            console.log("err", err);
+        }
+        console.log("shici", shici);
+        console.log("result", result);
+        console.log("result.insertedCount: ", result.insertedCount);
+        cb(result);
+    });
+};
+
+// client.connect(err => {
+//     if (err) {
+//         console.log("err", err);
+//         return;
+//     }
+//     const db = client.db(dbName);
+//     insertDocument(db, () => {
+//         client.close();
+//     });
+// });
+
+const insertDocumentForShici = shici => {
+    client.connect(err => {
+        if (err) {
+            console.log("err", err);
+            return;
+        }
+        const db = client.db(dbName);
+        insertDocument(db, shici, () => {
+            client.close();
+        });
+    });
+};
+
+module.exports = {
+    insertDocumentForShici
+};
