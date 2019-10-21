01a40aa1a61d979cae5d13f858e10559c70cf419
youling4438 <youling0106@qq.com>
2019-10-21 22:27:31 +0800

    :pencil:add request header type key

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.all("/*", function(req, res, next) {
     // 跨域处理
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, content-type"
+    );
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", " 3.2.1");
     res.header("Content-Type", "application/json;charset=utf-8");
